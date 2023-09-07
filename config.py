0b9d222fb8816383dfd0b4d6254e1daf193dfed0
zhilin1112 <87698109+zhilin1112@users.noreply.github.com>
2023-09-07 17:05:35 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.c2swkg3ucxtm.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "kongzhilin-employee"
-customregion = "us-east-1"
-
