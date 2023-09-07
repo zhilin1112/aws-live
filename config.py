89b7569bd6134989650cfb5831814c7bdc3eb8cc
zhilin1112 <87698109+zhilin1112@users.noreply.github.com>
2023-09-07 17:06:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c2swkg3ucxtm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "kongzhilin-employee"
+customregion = "us-east-1"
+
